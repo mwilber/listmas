bf8cc58062972818ad74b4a77647b971a7e4a3c5
Matt Wilber <mwilber@gmail.com>
2015-11-11 16:48:33 -0500

    more fb tweaking

--- index.php
+++ index.php
@@ -278,7 +278,7 @@
 			});
 			var docheight = $("body").height();
 			// Override detected height
-			docheight = 3000;
+			docheight = 3200;
 			FB.Canvas.setSize({ width: 810, height: docheight });
 			}
 		</script> 
